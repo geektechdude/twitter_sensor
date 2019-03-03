ad4002405bc7e43ef677567297913f6514d8cc15
GeekTechDude <34283325+geektechdude@users.noreply.github.com>
2019-03-03 10:20:14 +0000

    Update tweet_sensor.py

--- tweet_sensor.py
+++ tweet_sensor.py
@@ -1,6 +1,8 @@
 # geektechstuff
 # Twitter Application
 
+import time
+
 # gpiozero for CPU
 from gpiozero import CPUTemperature
 
@@ -15,6 +17,9 @@ try:
     from smbus2 import SMBus
 except ImportError:
     from smbus import SMBus
+    
+# lux sensor
+import ltr559
 
 # imports the Twitter API keys from a .py file called auth
 from auth import (
@@ -44,7 +49,7 @@ def cpu_temperature():
 
 def get_temp():
     temperature = bmp280.get_temperature()
-    temperature = round(temperature)
+    temperature = round((temperature),2)
     temperature = temperature -2
     temperature = str(temperature)
     return(temperature)
@@ -55,12 +60,22 @@ def get_pressure():
     pressure = str(pressure)
     return(pressure)
 
+def get_lux():
+    lux = ltr559.get_lux()
+    for x in range(1,5):
+        x_lux = lux
+        time.sleep(0.5)
+    lux_rounded = round(x_lux,2)
+    lux_str = str(lux_rounded)
+    return(lux_str)
+
 def write_tweet():
     get_temp()
     get_pressure()
     time_now()
-    cpu_temperature()               
-    tweet_to_send = "geektechstuff.com room currently at "+get_temp()+" degrees celesius.\nPressure is at "+get_pressure()+"  hectopascals.\nThe time is "+time_now()+"\nCPU temp is "+cpu_temperature()
+    cpu_temperature()
+    get_lux()
+    tweet_to_send = "geektechstuff.com room currently at "+get_temp()+" degrees celesius.\nPressure is at "+get_pressure()+"  hectopascals.\nThe time is "+time_now()+"\nCPU temp is "+cpu_temperature()+". Lux level in room "+get_lux()+" .\nCreated in #Python"
     tweet_to_send = str(tweet_to_send)
     twitter.update_status(status=tweet_to_send)
     return()
@@ -71,3 +86,5 @@ def time_now():
     return(now)
 
 write_tweet()
+
+
